c9810aab74c8fac12e0f157ccf23c08896f9c574
kmi0817 <kmi0817@naver.com>
2022-07-23 19:14:44 +0900

    feat: :truck: chagne some paths

--- routes/admin.js
+++ routes/admin.js
@@ -13,37 +13,37 @@ router.get("/users", async (req, res) => {
 
 // 후원 신청 현황
 router.get("/sponsorship", async (req, res) => {
-    res.render("users.html");
+    res.render("sponsorship.html");
 });
 
 // 자원봉사 신청 현황
 router.get("/volunteerwork", async (req, res) => {
-    res.render("users.html");
+    res.render("volunteerwork.html");
 });
 
 // 비밀상담 관리
 router.get("/counsel", async (req, res) => {
-    res.render(".html");
+    res.render("counsel.html");
 });
 
 // 공지사항 관리
 router.get("/board", async (req, res) => {
-    res.render(".html");
+    res.render("board.html");
 });
 
 // 시설 이미지 관리
 router.get("/images", async (req, res) => {
-    res.render(".html");
+    res.render("images.html");
 });
 
 // 자유 게시판 관리
 router.get("/community", async (req, res) => {
-    res.render(".html");
+    res.render("community.html");
 });
 
 // 포토 갤러리 관리
 router.get("/gallery", async (req, res) => {
-    res.render(".html");
+    res.render("gallery.html");
 });
 
 module.exports = router;
