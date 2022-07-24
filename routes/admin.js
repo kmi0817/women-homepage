b6b2ccd59310bbc60f1e96223a51a2f52d40fb86
kmi0817 <kmi0817@naver.com>
2022-07-24 13:48:43 +0900

    feat: 라우트에 페이지 연결
    라우트에 연결된 페이지는 예시 페이지로, 아직 작업이 진행되지 않음.

--- routes/admin.js
+++ routes/admin.js
@@ -38,37 +38,37 @@ router.get("/users", async (req, res) => {
 
 // 후원 신청 현황
 router.get("/sponsorship", async (req, res) => {
-    res.render("sponsorship.html");
+    res.render("sponsorship");
 });
 
 // 자원봉사 신청 현황
 router.get("/volunteerwork", async (req, res) => {
-    res.render("volunteerwork.html");
+    res.render("volunteerwork");
 });
 
 // 비밀상담 관리
 router.get("/counsel", async (req, res) => {
-    res.render("counsel.html");
+    res.render("counsel");
 });
 
 // 공지사항 관리
 router.get("/board", async (req, res) => {
-    res.render("board.html");
+    res.render("board");
 });
 
 // 시설 이미지 관리
 router.get("/images", async (req, res) => {
-    res.render("images.html");
+    res.render("images");
 });
 
 // 자유 게시판 관리
 router.get("/community", async (req, res) => {
-    res.render("community.html");
+    res.render("community");
 });
 
 // 포토 갤러리 관리
 router.get("/gallery", async (req, res) => {
-    res.render("gallery.html");
+    res.render("gallery");
 });
 
 // 회원 등록
