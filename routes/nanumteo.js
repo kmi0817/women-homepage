d017204af12c678daa670267cce4075b1c322074
kmi0817 <kmi0817@naver.com>
2022-07-25 23:07:02 +0900

    feat: save application form for sponsorship in DB
    일단 CMS 자동이체 관련 필드를 모두 채워야만 저장되도록 설정함. 이후에 자동이체신청/통장입금 시 CMS 관련 필드를 어떻게 처리할지 고민할 예정.

--- routes/nanumteo.js
+++ routes/nanumteo.js
@@ -1,11 +1,29 @@
-const { application } = require("express");
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
+const sanitizeHtml = require("sanitize-html");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "women"
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("mysql connection error");
+        console.log(err);
+        throw err;
+    } else {
+        console.log("DB OK");
+    }
+});
 
 router.get("/", async (req, res) => {
     res.send("나눔터");
 });
-
 
 router.get("/howto", async (req, res) => {
     if (req.query.nanum === "sponsorship") {
@@ -23,6 +41,21 @@ router.get("/applyfor", async (req, res) => {
 
 router.get("/counsel", async (req, res) => {
     res.send("비밀상담");
+});
+
+router.post("/process", async (req, res) => {
+    if (req.query.nanum === "sponsorship") {
+        
+        const sql = `INSERT INTO sponsorship(name, job, email, addr1, addr2, addr3, tel, recommender, purpose, method, withdrawal_bank, account, depositor, withdrawal_date, amount, period) VALUES('${sanitizeHtml(req.body.name)}', '${sanitizeHtml(req.body.job)}', '${sanitizeHtml(req.body.email)}', ${sanitizeHtml(req.body.addr1)}, '${sanitizeHtml(req.body.addr2)}', '${sanitizeHtml(req.body.addr3)}', '${sanitizeHtml(req.body.tel)}', '${sanitizeHtml(req.body.recommender)}', '${sanitizeHtml(req.body.purpose)}', '${sanitizeHtml(req.body.method)}', '${sanitizeHtml(req.body.withdrawal_bank)}', '${sanitizeHtml(req.body.account)}',  '${sanitizeHtml(req.body.depositor)}', ${sanitizeHtml(req.body.withdrawal_date)}, ${sanitizeHtml(req.body.amount)}, '${sanitizeHtml(req.body.period)}')`;
+        connection.query(sql, (err, ret, fields) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log("** an application for sponsorship has been saved in DB");
+                res.redirect("/nanumteo/applyfor?nanum=sponsorship");
+            };
+        });
+    }
 });
 
 module.exports = router;
