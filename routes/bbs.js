eb8cf3dcddf53c568fd86d79fffb8a1eb4c8d9fa
kmi0817 <kmi0817@naver.com>
2022-07-31 12:06:19 +0900

    feat: 각 게시판의 게시글 READ 추가

--- routes/bbs.js
+++ routes/bbs.js
@@ -7,20 +7,40 @@ const mysql = require("mysql");
 const config = require("../config.js");
 const connection = mysql.createConnection(config);
 
+/* 게시판 메인화면 겸 공지사항 페이지 */
 router.get("/", async (req, res) => {
-    res.send("게시판 메인화면 겸 공지사항 페이지");
+    const sql = `SELECT * FROM bbs WHERE bbs='board'`;
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
 });
 
+/* 시설 이미지 */
 router.get("/images", async (req, res) => {
-    res.send("시설 이미지");
+    const sql = `SELECT * FROM bbs WHERE bbs='images'`;
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
 });
 
+/* 자유 게시판 */
 router.get("/community", async (req, res) => {
-    res.send("자유 게시판");
+    const sql = `SELECT * FROM bbs WHERE bbs='community'`;
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
 });
 
+/* 포토 갤러리 */
 router.get("/gallery", async (req, res) => {
-    res.send("포토 갤러리");
+    const sql = `SELECT * FROM bbs WHERE bbs='gallery'`;
+    connection.query(sql, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    });
 });
 
 module.exports = router;
