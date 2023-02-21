9aad0bf3e2e0703760f20e312345e686f1dfda82
YejinHwang-D <hyj3463@naver.com>
2023-02-21 22:15:11 +0900

    feat: 퀵 메뉴 생성 완료

--- front-end/src/pages/MainComponent/MenuButton.ts
+++ front-end/src/pages/MainComponent/MenuButton.ts
@@ -19,8 +19,10 @@ export class MenuButton {
         div1.setAttribute('class', 'div1');
         const div1Div1 = document.createElement('div');
         div1Div1.setAttribute('class', 'div1Div1');
+        div1Div1.innerText = `소소뜨라`;
         const div1Div2 = document.createElement('div');
         div1Div2.setAttribute('class', 'div1Div2');
+        div1Div2.innerText = `비밀상담`;
 
         div1.appendChild(div1Div1);
         div1.appendChild(div1Div2);
@@ -29,10 +31,13 @@ export class MenuButton {
         div2.setAttribute('class', 'div2');
         const div2Div1 = document.createElement('div');
         div2Div1.setAttribute('class', 'div2Div1');
+        div2Div1.innerText = `프로그램 안내`;
         const div2Div2 = document.createElement('div');
         div2Div2.setAttribute('class', 'div2Div2');
+        div2Div2.innerText = `후원 안내 및 신청`;
         const div2Div3 = document.createElement('div');
         div2Div3.setAttribute('class', 'div2Div3');
+        div2Div3.innerText = `자원봉사 안내 및 신청`;
 
         div2.appendChild(div2Div1);
         div2.appendChild(div2Div2);
@@ -42,10 +47,13 @@ export class MenuButton {
         div3.setAttribute('class', 'div3');
         const div3Div1 = document.createElement('div');
         div3Div1.setAttribute('class', 'div3Div1');
+        div3Div1.innerText = `자유게시판`;
         const div3Div2 = document.createElement('div');
         div3Div2.setAttribute('class', 'div3Div2');
+        div3Div2.innerText = `시설이미지`;
         const div3Div3 = document.createElement('div');
         div3Div3.setAttribute('class', 'div3Div3');
+        div3Div3.innerText = `포토갤러리`;
 
         div3.appendChild(div3Div1);
         div3.appendChild(div3Div2);
