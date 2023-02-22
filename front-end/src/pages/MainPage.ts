c9d058ffaba84c77bb6917926a2da379856548f4
YejinHwang-D <hyj3463@naver.com>
2023-02-23 00:17:11 +0900

    feat: 핵심가치 구현 완료

--- front-end/src/pages/MainPage.ts
+++ front-end/src/pages/MainPage.ts
@@ -4,6 +4,10 @@ import { MenuButton } from './MainComponent/MenuButton.js';
 import { MissionVision } from './MainComponent/MissionVision.js';
 import { NewNotice } from './MainComponent/NewNotice.js';
 import { NewPost } from './MainComponent/NewPost.js';
+import { Value } from './MainComponent/Value.js';
+
+import { makeElement } from '../api/api.js';
+
 
 export class MainPage {
     constructor(private $target: HTMLElement) {
@@ -11,14 +15,11 @@ export class MainPage {
 
     render() {
         // main_section
-        const main_section = document.createElement('section');
-        main_section.setAttribute('class', 'main_section');
+        const main_section = makeElement('section', 'class', 'main_section')
         this.$target.appendChild(main_section);
 
-        const main_section_first = document.createElement('div');
-        main_section_first.setAttribute('class', 'main_section_first');
-        const main_section_second = document.createElement('div');
-        main_section_second.setAttribute('class', 'main_section_second');
+        const main_section_first = makeElement('div', 'class', 'main_section_first')
+        const main_section_second = makeElement('div', 'class', 'main_section_second');
         main_section.appendChild(main_section_first);
         main_section.appendChild(main_section_second);
 
@@ -28,15 +29,16 @@ export class MainPage {
         const newNotice = new NewNotice(main_section_second);
         const mainDonation = new MainDonation(main_section_second);
 
-
         // mission_section
-        const mission_section = document.createElement('section');
-        mission_section.setAttribute('class', 'mission_section');
+        const mission_section = makeElement('section', 'class', 'mission_section');
         this.$target.appendChild(mission_section);
         const missionVision = new MissionVision(mission_section);
 
-
         // value_section
+        const value_section = makeElement('section', 'class', 'value_section');
+        this.$target.appendChild(value_section);
+        const value = new Value(value_section);
+
         // info_section
         // support_section
         // history_section
