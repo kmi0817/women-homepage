79e4ae9775fd442e4c0ee8246597ee34dbee56c2
YejinHwang-D <hyj3463@naver.com>
2023-02-22 17:09:24 +0900

    style: 메인페이지 스타일 재적용

--- front-end/build/pages/MainPage.js
+++ front-end/build/pages/MainPage.js
@@ -8,10 +8,28 @@ export class MainPage {
         this.$target = $target;
     }
     render() {
-        const imageSlide = new ImageSlide(this.$target);
-        const menuButton = new MenuButton(this.$target);
-        const newPost = new NewPost(this.$target);
-        const newNotice = new NewNotice(this.$target);
-        const mainDonation = new MainDonation(this.$target);
+        // main_section
+        const main_section = document.createElement('section');
+        main_section.setAttribute('class', 'main_section');
+        this.$target.appendChild(main_section);
+        const main_section_first = document.createElement('div');
+        main_section_first.setAttribute('class', 'main_section_first');
+        const main_section_second = document.createElement('div');
+        main_section_second.setAttribute('class', 'main_section_second');
+        main_section.appendChild(main_section_first);
+        main_section.appendChild(main_section_second);
+        const imageSlide = new ImageSlide(main_section_first);
+        const menuButton = new MenuButton(main_section_first);
+        const newPost = new NewPost(main_section_second);
+        const newNotice = new NewNotice(main_section_second);
+        const mainDonation = new MainDonation(main_section_second);
+        // mission_section
+        // const mission_section = document.createElement('section');
+        // mission_section.setAttribute('class', 'mission_section');
+        // this.$target.appendChild(mission_section);
+        // value_section
+        // info_section
+        // support_section
+        // history_section
     }
 }
