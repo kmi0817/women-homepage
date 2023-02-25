861e997e6a1a59b82261711e5b4204fe956003d6
YejinHwang-D <hyj3463@naver.com>
2023-02-25 22:14:41 +0900

    feat: 지원사업 영역 생성(모달 제외)

--- front-end/build/pages/MainComponent/MainInfo.js
+++ front-end/build/pages/MainComponent/MainInfo.js
@@ -1,9 +1,39 @@
+import { makeElement } from '../../api/api.js';
 export class MainInfo {
     constructor(appRoot) {
         this.appRoot = appRoot;
+        this.textData = [
+            { title: '지원 프로그램', desc: '탈성매매 지원, 법률 지원, 의료 지원, 창업지원' },
+            { title: '상담 프로그램', desc: '전문심리상담, 심리 검사, 집단상담' },
+            { title: '성장을 위한 프로그램', desc: '참된 만남과 성장을 위한 문화예술치유, 교육, 생활 지도' },
+            { title: '취미 프로그램', desc: '여유로운 생활을 위한 취미 활동' },
+        ];
         this.render();
     }
     render() {
-        console.log('main INfo!');
+        const title_section = makeElement('section', 'class', 'info_title_section');
+        const title_title = makeElement('p', 'class', 'info_title');
+        title_title.innerText = `한국여성의집 소개`;
+        const title_desc = makeElement('p', 'class', 'info_title desc');
+        title_desc.innerText = `탈성매매여성과 함께 어제의 아픔을 한 움큼 비우고 오늘의 땀으로 꿈을 빚어 내일의 희망을 나누고자 합니다.`;
+        title_section.appendChild(title_title);
+        title_section.appendChild(title_desc);
+        const desc_section = makeElement('section', 'class', 'info_desc_section');
+        for (let val of this.textData) {
+            const ele = this.element(val.title, val.desc);
+            desc_section.appendChild(ele);
+        }
+        this.appRoot.appendChild(title_section);
+        this.appRoot.appendChild(desc_section);
+    }
+    element(title, desc) {
+        const div = makeElement('div', 'class', 'info_ele_div');
+        const titleDiv = makeElement('div', 'class', 'info_ele_title');
+        const descDiv = makeElement('div', 'class', 'info_ele_desc');
+        titleDiv.innerText = title;
+        descDiv.innerText = desc;
+        div.appendChild(titleDiv);
+        div.appendChild(descDiv);
+        return div;
     }
 }
--- front-end/src/pages/MainComponent/MainSupport.ts
+++ front-end/src/pages/MainComponent/MainSupport.ts
@@ -1,9 +1,94 @@
+import { makeElement } from '../../api/api.js';
+
+type supportDataType = {
+    title: string,
+    info: string,
+    list: Array<string>,
+    supportText: string,
+}
+
 export class MainSupport {
+    private modalData: Array<supportDataType> = [
+        {
+            title: '법률지원',
+            info: '그동안 빚 때문에 마음고생 많으셨죠?',
+            list: ['선불금으로 인한 고소 및 고발', '민형사 소송', '파산 및 신용회복'],
+            supportText: '전문적인 법률 지원으로 여러분이 탈성매매하도록 도와드립니다.',
+        },
+        {
+            title: '의료지원',
+            info: '그동안 빚 때문에 마음고생 많으셨죠?',
+            list: ['선불금으로 인한 고소 및 고발', '민형사 소송', '파산 및 신용회복'],
+            supportText: '전문적인 법률 지원으로 여러분이 탈성매매하도록 도와드립니다.',
+        },
+        {
+            title: '자활지원',
+            info: '그동안 빚 때문에 마음고생 많으셨죠?',
+            list: ['선불금으로 인한 고소 및 고발', '민형사 소송', '파산 및 신용회복'],
+            supportText: '전문적인 법률 지원으로 여러분이 탈성매매하도록 도와드립니다.',
+        },
+        {
+            title: '진학지원',
+            info: '그동안 빚 때문에 마음고생 많으셨죠?',
+            list: ['선불금으로 인한 고소 및 고발', '민형사 소송', '파산 및 신용회복'],
+            supportText: '전문적인 법률 지원으로 여러분이 탈성매매하도록 도와드립니다.',
+        },
+        {
+            title: '취미/문화관련',
+            info: '그동안 빚 때문에 마음고생 많으셨죠?',
+            list: ['선불금으로 인한 고소 및 고발', '민형사 소송', '파산 및 신용회복'],
+            supportText: '전문적인 법률 지원으로 여러분이 탈성매매하도록 도와드립니다.',
+        },
+        {
+            title: '정서지원사업',
+            info: '그동안 빚 때문에 마음고생 많으셨죠?',
+            list: ['선불금으로 인한 고소 및 고발', '민형사 소송', '파산 및 신용회복'],
+            supportText: '전문적인 법률 지원으로 여러분이 탈성매매하도록 도와드립니다.',
+        },
+    ]
+
     constructor(private appRoot: HTMLElement) {
         this.render();
     }
 
     render() {
-        console.log('main support!');
+        const title_section = makeElement('div', 'class', 'support_title_section');
+        const title_title = makeElement('p', 'class', 'support_title');
+        title_title.innerText = `지원사업`;
+        title_section.appendChild(title_title);
+        const title_title_desc = makeElement('p', 'class', 'support_title desc');
+        title_title_desc.innerText = `한국여성의집에서 지원하는 다양한 사업`;
+        title_section.appendChild(title_title_desc);
+        this.appRoot.appendChild(title_section);
+
+        const title_desc = makeElement('div', 'class', 'support_desc');
+        for (let val of this.modalData) {
+            const ele = this.element(val);
+            title_desc.appendChild(ele);
+        }
+        this.appRoot.appendChild(title_desc);
+
+        const bottom = makeElement('div', 'class', 'support_bottom');
+        bottom.innerText = `사업의 자세한 내용을 확인하려면 해당 영역을 클릭해주세요.`;
+        this.appRoot.appendChild(bottom);
+    }
+
+    element(modalData: supportDataType) {
+        const ele = makeElement('div', 'class', 'support_ele_section');
+        const titleDiv = makeElement('div', 'class', 'support_ele_title');
+        const titleClick = makeElement('div', 'class', 'support_ele_click');
+        titleDiv.innerText = modalData.title;
+        titleClick.innerText = `click!`;
+
+        ele.addEventListener('click', () => {
+            this.createModal(modalData);
+        });
+        ele.appendChild(titleDiv);
+        ele.appendChild(titleClick);
+        return ele;
+    }
+
+    createModal(modalData: supportDataType) {
+        alert('modal!');
     }
 }
