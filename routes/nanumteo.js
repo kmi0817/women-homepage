eaa0b21728480ec40da0509bb210ebc060c7662d
kmi0817 <kmi0817@naver.com>
2023-03-13 18:02:56 +0900

    chore: if문의 { } 정리

--- routes/nanumteo.js
+++ routes/nanumteo.js
@@ -15,17 +15,17 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/howto", async (req, res) => {
-    if (req.query.nanum === "sponsorship") {
+    if (req.query.nanum === "sponsorship")
         res.send("후원 안내");
-    } else if (req.query.nanum === "volunteerwork")
-    res.send("자원봉사 안내");
+    else if (req.query.nanum === "volunteerwork")
+        res.send("자원봉사 안내");
 });
 
 router.get("/applyfor", async (req, res) => {
-    if (req.query.nanum === "sponsorship") {
+    if (req.query.nanum === "sponsorship")
         res.send("후원 신청");
-    } else if (req.query.nanum === "volunteerwork")
-    res.send("자원봉사 신청");
+    else if (req.query.nanum === "volunteerwork")
+        res.send("자원봉사 신청");
 });
 
 router.get("/counsel", async (req, res) => {
