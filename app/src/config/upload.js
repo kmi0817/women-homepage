73503fd2e36d2fe4375f023e5345795adfcd3859
kmi0817 <kmi0817@naver.com>
2023-03-24 19:56:34 +0900

    refactor: optimize codes related to multer & change upload path

---
+++ app/src/config/upload.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        return cb(null, "./src/uploads");
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        return cb(null, `${file.fieldname}_${Date.now()}${ext}`);
+    }
+});
+
+// extension filter
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/gif" ||
+        file.mimetype === "application/x-hwp, application/haansofthwp, application/vnd.hancom.hwp" ||
+        file.mimetype === "application/pdf" ||
+        file.mimetype === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+        file.mimetype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        ) {
+            return cb(null, true);
+        } else {
+            req.fileValidationError = "Only png, jpg/jpeg, gig, hwp, pdf, xlxs, docx available";
+            return cb(null, false, new Error("INVALID_EXTENSION"));
+        }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 30 * 1024 * 1024 },
+}).array("attachedFiles", 5); // maxCount 5
+
+const uploadErrHandle = (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.json({ success: false, err });
+        } else if(req.fileValidationError) { // Invalid file type
+            return res.json({ success: false, err: { "name": "MulterError", "message": "INVALID_EXTENSION", "code": req.fileValidationError } });
+        } else {
+            return next();
+        }
+    });
+};
+
+module.exports = uploadErrHandle;
--- app/src/routes/community/notice.js
+++ app/src/routes/community/notice.js
@@ -1,45 +1,22 @@
 "use strict";
 
 const express = require("express");
-const multer = require("multer");
-const path = require("path");
 const router = express.Router();
 
 const noticeController = require("../../controllers/community/notice.controller");
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        return cb(null, "./src/public/uploads/");
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        return cb(null, `${file.fieldname}_${Date.now()}${ext}`);
-    }
-});
-
-const upload = multer({
-    storage: storage
-}).array("attachedFiles", 5); // maxCount 5
+const uploadErrHandle = require("../../config/upload");
 
 router.get("/", noticeController.output.notice);
 router.get("/:id", noticeController.output.noticeId);
 
 router.post(
     "/register",
-    (req, res, next) => {
-        upload(req, res, (err) => {
-            return err ? res.json({ success: false, err }) : next();
-        });
-    },
+    uploadErrHandle,
     noticeController.process.register
 );
 router.patch(
     "/modify",
-    (req, res, next) => {
-        upload(req, res, (err) => {
-            return err ? res.json({ success: false, err }) : next();
-        });
-    },
+    uploadErrHandle,
     noticeController.process.modify
 );
 router.delete("/remove", noticeController.process.remove);
