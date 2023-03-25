ed6058941d3f58b0206cf661dc25d3643779a567
kmi0817 <kmi0817@naver.com>
2023-03-25 16:01:40 +0900

    feat: :poop: load history, notice(10), free(10) data on Main Page

--- app/src/controllers/main.controller.js
+++ app/src/controllers/main.controller.js
@@ -1,11 +1,27 @@
 "use strict";
 
 const History = require("../models/History");
+const Notice = require("../models/community/Notice");
+const Free = require("../models/community/Free");
 
 const output = {
     main: async (req, res) => {
+        req.body["pageNo"] = Number(req.query.pageNo) || 1;
+
         const history = new History(req.body);
-        const response = await history.show();
+        const response1 = await history.show();
+        const notice = new Notice(req.body);
+        const response2 = await notice.show();
+        const free = new Free(req.body);
+        const response3 = await free.show();
+
+        // Combine data
+        const response = {
+            history: response1,
+            notice: response2,
+            free: response3
+        }
+
         return res.json(response);
     },
     sostt: async (req, res) => {
